caf63c66da10ef48c5a0f0c1a338d334d4df9d7e
Andrew Werner <alrw@stripe.com>
2020-11-16 08:18:19 +0000

    [AW][wip] another quick update to to the csv test

--- spec/unit/csv_data_spec.rb
+++ spec/unit/csv_data_spec.rb
@@ -5,11 +5,13 @@ require 'monte/csv_data'
 class SUT; end
 
 RSpec.describe CSVData do
+  before(:each) do
+    @sut = SUT.new
+    @sut.extend(CSVData)
+  end
   it 'given a csv it returns an array of weekly throughput' do
-    before(:each) do
-      @sut = SUT.new
-      @sut.extend(CSVData)
-      expect('OK').to eq('Not Implemented')
-    end
+    expected = [1, 2]
+    puts(File.join(File.dirname(__FILE__)))
+    expect('OK').to eq('Not Implemented')
   end
 end
